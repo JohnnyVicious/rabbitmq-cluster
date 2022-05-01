d4a471b86bf3528ab577b1ff2eabeb88ab801895
Johnny <6608571+JohnnyVicious@users.noreply.github.com>
2022-05-01 20:14:44 +0200

    Update startrabbit.sh

--- startrabbit.sh
+++ startrabbit.sh
@@ -40,8 +40,6 @@ echo "HOSTNAME " $HOSTNAME
 echo ""
 echo ""
 echo "Starting RabbitMQ Server For host: " $HOSTNAME
-systemd-resolved --status | grep -B 9 -A 6 "Current DNS Server"
-host -v something.unknown | awk -F "[ #]" '/Received /{print$5}' | uniq
 change_default_user() {
   # change default user only if ENV is provided
   if [ -z $RABBITMQ_DEFAULT_USER ] && [ -z $RABBITMQ_DEFAULT_PASS ]; then
